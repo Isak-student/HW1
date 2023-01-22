2d586555aa30c672f308e4f46e7210a13977bc27
Isak <zakusi@bk.ru>
2023-01-22 12:19:23 +0530

    Выполнил дз 1

---
+++ Zadacha001/Program.cs
@@ -0,0 +1,21 @@
+﻿int a, b;
+string userEnter1, userEnter2;
+
+Console.Write("Введите первое число: ");
+userEnter1 = Console.ReadLine()!;
+
+a = int.Parse(userEnter1);
+
+Console.Write("Введите второе число: ");
+userEnter2 = Console.ReadLine()!;
+
+b = int.Parse(userEnter2);
+
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
