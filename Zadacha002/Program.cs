6dd8d2ec2d4e3f5bf584de338e5721b8dfe3f562
Isak <zakusi@bk.ru>
2023-01-23 11:06:50 +0530

    Сделал 3 задачу

---
+++ Zadacha002/Program.cs
@@ -0,0 +1,20 @@
+﻿int a, b, c;
+
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out c);
+
+int max = a;
+if(b > max)
+max = b;
+if(c > max)
+max = c;
+
+Console.WriteLine($"Максимальное число: {max}");
